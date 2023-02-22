eb8bc430a6abc24b5654b18be56f1c6640244532
rvdiaz <valdesdiazroberto@gmail.com>
2023-02-22 15:25:53 -0500

    adding bucket s3 config to middleware

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,31 @@
 module.exports = [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'strapi-aws-s3-images-bucket.s3.eu-west-2.amazonaws.com', // change here
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'strapi-aws-s3-images-bucket.s3.eu-west-2.amazonaws.com', // change here
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
