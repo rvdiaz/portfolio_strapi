960143ba4e470e5f77ee84539e3f07dea51478e0
rvdiaz <valdesdiazroberto@gmail.com>
2023-03-16 07:58:34 -0400

    adding website to email answer

--- src/api/lead/content-types/lead/lifecycles.js
+++ src/api/lead/content-types/lead/lifecycles.js
@@ -7,8 +7,8 @@ module.exports={
             sendEmail({
                 from: 'rvaldez@305plasticsurgery.com',
                 to: 'valdesdiazroberto@gmail.com',
-                subject: 'hey',
-                html: '<p>Name:'+result.name+'</p></br><p>Email:'+result.email+'</p></br><p>Phone:'+result.phone+'</p></br><p>Message:'+result.message+'</p></br>'
+                subject: 'New lead in Portfolio Online',
+                html: '<p>Name:'+result.name+'</p></br><p>Email:'+result.email+'</p></br><p>Phone:'+result.phone+'</p></br><p>Message:'+result.website+'</p></br><p>Message:'+result.message+'</p></br>'
             })
         } catch (error) {
             console.log(error);
